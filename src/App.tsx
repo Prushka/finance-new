98f50f4e16e2e9469d281ee82afb7d0d75131d71
Dan Lyu <dan@lyu.sh>
2024-08-01 14:17:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+    return (
+   <main className={"w-full h-full flex justify-center items-center"}>
+
+       <Button>Test</Button>
+   </main>
+    )
+}
+
+export default App
